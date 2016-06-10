ec71e77d0487fb9c098daf64e2cb8a3743629ab2
Will Pearson <pearswj@gmail.com>
2016-06-10 10:22:23 +0100

    Initial commit

---
+++ Md5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Md5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            foreach (string path in args)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                string hash;
+                using (var md5 = System.Security.Cryptography.MD5.Create())
+                {
+                    using (var stream = File.OpenRead(path))
+                    {
+                        hash = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty).ToLower(); // unix-y md5 hash
+                    }
+                }
+
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+
+                Console.WriteLine(string.Format("{0}\t{1}\t{2}ms", path, hash, elapsedMs));
+            }
+        }
+    }
+}
